22ad12faa5784c4f889788862fa3e8fdeb7cd7e1
TTuna <t.tunakoseler@gmail.com>
2022-12-20 00:17:09 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //Sort
+        int[] sayiDizisi = {23,12,4,86,72,3,11,17};
+        
+        Console.WriteLine("***Sırasız***");
+        foreach (var sayi in sayiDizisi)
+        {
+            Console.WriteLine(sayi);
+        }
+
+        Console.WriteLine("***Sıralı***");
+        Array.Sort(sayiDizisi);
+        foreach (var sayi in sayiDizisi)
+        {
+            Console.WriteLine(sayi);
+        }
+
+        //Clear
+        Console.WriteLine("***Array Clear***");
+        Array.Clear(sayiDizisi,2,2);                //sayıdizisi elemanlarını kullanarak 2. indexten itibaren 2 tane elemanı sıfırlar
+        foreach (var sayi in sayiDizisi)
+        {
+            Console.WriteLine(sayi);
+        }
+
+        //Reverse
+        Console.WriteLine("***Array REVERSE***");
+        Array.Reverse(sayiDizisi);
+        foreach (var sayi in sayiDizisi)
+        {
+            Console.WriteLine(sayi);
+        }
+
+        //IndexOf
+        Console.WriteLine("***Array INDEXOF***");
+        Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+
+        //Resize
+        Console.WriteLine("***Array RESIZE***");
+        Array.Resize<int>(ref sayiDizisi,9);
+        foreach (var sayi in sayiDizisi)
+        {
+            Console.WriteLine(sayi);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# ARRAY METHODS (www.patikadev.com)
+
+---
+
+1. Sort
+2. Clear
+3. Reverse
+4. IndexOf
+5. Resize<>
